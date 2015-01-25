d43ba3fca2dfc7f7a80de27c35489c2fbbecfab3
goduwin-ravi <goduwin_ravi@yahoo.com>
2015-01-25 11:18:56 -0600

    Update-2
    Update-2

--- server.R
+++ server.R
@@ -11,29 +11,26 @@ mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-
+  
   # Compute the forumla text in a reactive expression since it is 
   # shared by the output$caption and output$mpgPlot expressions
   formulaText <- reactive({
-    paste("mpg ~", input$variable)
+    paste("mpg ~ ", input$variable)
   })
 
   # Return the formula text for printing as a caption
   output$caption <- renderText({
+    # Filter display data as required by the user
+    mpgData <- mpgData[ which(mpgData$am==input$am & mpgData==input$cyl), ]
     formulaText()
   })
-
-  # Generate a plot of the requested variable against mpg and only 
-  # include outliers if requested
-  output$mpgPlot <- renderPlot({
+  
+  # Generate a plot of the requested variable against mpg and  
+  # include outliers only if requested
+  output$mpgPlot <- renderPlot({    
     boxplot(as.formula(formulaText()), 
             data = mpgData,
             col = (c("red","blue")),
             outline = input$outliers)
-
-    pairs(as.formula(formulaText()), 
-          data = mpgData, 
-          col = (c("red","blue")),
-          outline = input$outliers)
   })
 })
--- ui.R
+++ ui.R
@@ -8,14 +8,32 @@ shinyUI(pageWithSidebar(
   # Application title
   headerPanel("Miles Per Gallon"),
 
-  sidebarPanel(
-   selectInput("variable", "Variable:",
-                list("Cylinders" = "cyl", 
-                     "Transmission" = "am", 
+  sidebarPanel( 
+    helpText("Analyze MPG data by a given parameter. 
+             The scope of data may be restricted to a specific number of cylinders (4,6, or 8)
+             and also to the type of transmission (Manual or Automatic). 
+             Optiionally, you may also include or exclude outlier data from your analysis."),
+    
+    selectInput("variable", "Parameter:",
+                list("Cyliners" = "cyl",
+                     "Transmission Type" = "am",
                      "Gears" = "gear",
                      "Weight" = "wt",
                      "Horse Power" = "hp")),
 
+    br(),
+    
+    sliderInput("cyl", "Cylinders:", 
+                min=4, max=8, step=2, value=4),
+    
+    br(),
+    
+    radioButtons("am", "Transmission type:",
+                 list("Automatic" = "0",
+                      "Manual" = "1")),
+    
+    br(),
+    
     checkboxInput("outliers", "Show outliers", FALSE)),
 
   mainPanel(
